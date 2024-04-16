00cdd9575e1635b4c81cf8061a88d4cdb31034e4
Julio Cezar Trajano dos Santos <jcezar0123@hotmail.com>
2024-04-16 19:24:01 -0300

    exercicio4

--- exercicio4.js
+++ exercicio4.js
@@ -3,6 +3,7 @@ const prompt = require('prompt-sync')();
 let escolha = 0;
 let contador = 0;
 let somaDasNotas = 0;
+
 while (escolha !== 3) {
     console.log(`
         1 - Adicionar nota
@@ -10,13 +11,16 @@ while (escolha !== 3) {
         3 - Sair
     `);
     escolha = parseInt(prompt('Escolha uma opção: '));
-    if (escolha === 1){
+    if (escolha === 1) {
         let nota = Number(prompt('Digite a nota: '));
         somaDasNotas += nota;
         contador++;
-    } else if (escolha === 2){
+    } else if (escolha === 2) {
         let media = somaDasNotas / contador;
         console.log(`A média das notas é ${media}`);
+        media >= 6.0 ? console.log('PARABÉNS! Você foi aprovado! :D') : console.log('REPROVADO! :(');
+    } else {
+        console.log('Opção inválida!');
     }
 }
 console.log('Programa finalizado!');
